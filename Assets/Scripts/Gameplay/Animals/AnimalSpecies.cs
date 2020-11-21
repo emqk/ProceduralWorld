6eea3bbdafda02ffaf063ca3a7fd5072c20e6553
Ja Fajny <jendromij@gmail.com>
2020-11-21 18:52:07 +0100

    Final

--- Assets/Scripts/Gameplay/Animals/AnimalSpecies.cs
+++ Assets/Scripts/Gameplay/Animals/AnimalSpecies.cs
@@ -26,7 +26,7 @@ public class AnimalSpecies : MonoBehaviour
 
         animalType = (AnimalSettings.AnimalType)Mathf.RoundToInt(Random.Range(0f, 1f));
         AnimalSettings animalSettings = new AnimalSettings(animalType, Random.Range(1, 4), new Vector3(1, 1, Random.Range(1.5f, 3.5f)));
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < 5; i++)
         {
             CreateNewAnimal(animalSettings);
         }
@@ -55,7 +55,7 @@ public class AnimalSpecies : MonoBehaviour
     {
         Building building = Instantiate(VegetationGenerator.instance.generatedBuildingPrefab);
         VegetationGenerator.instance.PlaceOnTerrainOnRandomPosInCircle(building.transform, transform.position, buildingMaxDistanceFromOrigin * WorldGenerator.GetScaleMultiplier());
-        building.mainBlock.transform.localScale = new Vector3(10, 10, 6) * WorldGenerator.GetScaleMultiplier();
+        building.mainBlock.transform.localScale = new Vector3(8, 8, 5) * WorldGenerator.GetScaleMultiplier();
         building.gameObject.AddComponent<NavMeshObstacle>().carving = true;
         building.GetComponent<NavMeshObstacle>().carveOnlyStationary = true;
         building.GetComponent<NavMeshObstacle>().size = building.mainBlock.transform.lossyScale - new Vector3(0.5f, 0, 0.5f);
@@ -113,7 +113,7 @@ public class AnimalSpecies : MonoBehaviour
         GameObject newAnimal = Instantiate(animalPrefabToSpawn.gameObject);
         NavMeshAgent animalAgent = newAnimal.GetComponent<NavMeshAgent>();
         if(animalAgent)
-            animalAgent.Warp(transform.position);
+            animalAgent.Warp(transform.position + new Vector3(0, 1, 0));
         else
             newAnimal.transform.position = transform.position;
         newAnimal.transform.SetParent(TerrainGenerator.instance.transform.parent);
